93f7c0fd8bf7b47c0ec028cd3a3f7f1298e0a663
MosheWork <88563378+MosheWork@users.noreply.github.com>
2022-10-30 09:43:06 +0200

    added customer edit and delete

--- client/src/components/CustomerTable.jsx
+++ client/src/components/CustomerTable.jsx
@@ -1,38 +1,44 @@
-import React from 'react'
-import SingleCustomer from './SingleCustomer';
+import React from "react";
+import SingleCustomer from "./SingleCustomer";
 import { Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 function CustomerTable(props) {
-
   return (
     <Table striped bordered hover size="sm">
-          <thead>
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>email</th>
+          <th>phone</th>
+          <th>address</th>
+          <th>Customer Created</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map((item) => {
+          return (
             <tr>
-              <th>name</th>
-              <th>email</th>
-              <th>phone</th>
-              <th>address</th>
-              <th>Customer Created</th>
+              <td>
+                <SingleCustomer customer={item}></SingleCustomer>
+              </td>
+              <td>{item.email}</td>
+              <td>{item.phone}</td>
+              <td>{item.address}</td>
+              <td>{new Date(item.createdAt).toLocaleDateString("he-IL")}</td>
+              <td>
+                <Link to={`/serviceCall/updateCustomer/${item._id}`}>
+                  <Button variant="primary">Edit Customer</Button>
+                </Link>
+                ;
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {props.data.map((item) => {
-              return (
-                <tr>
-                  <td>
-                    <SingleCustomer customer={item}></SingleCustomer>
-                  </td>
-                  <td>{item.email}</td>
-                  <td>{item.phone}</td>
-                  <td>{item.address}</td>
-                  <td>{new Date(item.createdAt).toLocaleDateString
-                  ('he-IL')}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-  )
+          );
+        })}
+      </tbody>
+    </Table>
+  );
 }
 
-export default CustomerTable
+export default CustomerTable;
